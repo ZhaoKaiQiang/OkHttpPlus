4fcb5a1d5d1eb5acc246b5d0ce32a77950c06df9
ZhaoKaiQiang <419927089@qq.com>
2015-11-16 17:52:05 +0800

    修改APP信息

---
+++ sample/src/main/java/com/socks/sample/okhttp/User.java
@@ -0,0 +1,23 @@
+package com.socks.sample.okhttp;
+
+public class User {
+
+    public String username;
+    public String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
